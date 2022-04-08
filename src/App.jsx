38b1725da172261cddea4a057a12daafdd1649b1
qendriimrexhepi <qendriimrexhepi@gmail.com>
2022-04-08 14:58:35 +0200

    My personal Portfolio with reactjs

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Header from "./components/header/Header"
+import About from "./components/about/About"
+import ProductList from "./components/productList/ProductList"
+import Contact from "./components/contact/Contact"
+import Toggle from "./components/toggle/Toggle"
+import { useContext } from "react"
+import { ThemeContext } from "./context";
+
+const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div       
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle />
+      <Header />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "../../img/movieticketbooking.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+  {
+    id: 2,
+    img: "./img/carRental.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+  {
+    id: 3,
+    img: "../img/5yjeGatimi.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+  {
+    id: 4,
+    img: "../img/projekti-SMP.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+  {
+    id: 5,
+    img: "../img/rentaCar.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+  {
+    id: 6,
+    img: "../img/apple.png",
+    link: "https://github.com/qendriimrexhepi",
+  },
+];
